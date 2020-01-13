547a792d222362459c3e81a840f2bbf7410ee3f4
Sandra Romero <romeroortiz.sandra@gmail.com>
2020-01-13 08:34:28 +0100

    Initial commit

---
+++ docs/precache-manifest.a4004a1a2c8c26bfd5f400267476e105.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e554746bc962a8b0cea438b4872e8246",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7fd58c5ca5b8a3480909",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "dac1ab6ea4f13e70f14f",
+    "url": "./static/js/2.91a6327e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.91a6327e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7fd58c5ca5b8a3480909",
+    "url": "./static/js/main.057e15a5.chunk.js"
+  },
+  {
+    "revision": "5a3cbef6df50c529659e",
+    "url": "./static/js/runtime-main.7e4cfb52.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
